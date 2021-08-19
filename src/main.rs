0466fa32876cb045b41bf04a6fd9e79d6b6cd6b2
david <david>
2021-08-19 09:45:46 +0200

    Edit distance backtracking

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use serde_json::{Result, Value};
 use std::cmp;
 use std::collections::HashMap;
+use std::fmt;
 
 #[derive(Clone, Debug)]
 enum JsonV<'a> {
@@ -37,6 +38,34 @@ enum DiffType2<'a> {
 enum Either<L: std::clone::Clone, R: std::clone::Clone> {
     Left(L),
     Right(R),
+}
+
+#[derive(Clone, Debug)]
+enum EditType {
+    Insert(usize),
+    Delete(usize),
+    Substitute(usize, usize, bool),
+    Unknown,
+}
+
+impl Default for EditType {
+    fn default() -> Self {
+        EditType::Unknown
+    }
+}
+
+impl fmt::Display for EditType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let v = match self {
+            EditType::Insert(x) => format!("Insert({})", x),
+            EditType::Delete(x) => format!("Delete({})", x),
+            EditType::Substitute(x, y, is_same) => {
+                format!("Substitute({}, {}, is_same = {})", x, y, is_same)
+            }
+            EditType::Unknown => "Unknown".to_string(),
+        };
+        write!(f, "{}", v)
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -258,27 +287,39 @@ fn edit_distance<T: std::cmp::Eq>(s1: Vec<T>, s2: Vec<T>) -> Vec<Vec<usize>> {
     let h = s1.len() + 1;
     let w = s2.len() + 1;
     let mut m: Vec<usize> = vec![Default::default(); w * h];
-    let mut backtrack: Vec<(i32, i32)> = vec![Default::default(); w * h];
+    // Left is insert, up is delete, and diagonal is substitution
+    let mut backtrack: Vec<EditType> = vec![Default::default(); w * h];
+    let mut backtrack2: Vec<(i32, i32)> = vec![Default::default(); w * h];
     let get = |x: usize, y: usize| x + (y * w);
     // Init
     for i in 0..h {
         m[get(0, i)] = i;
+        backtrack[get(0, i)] = EditType::Delete(if i > 0 { i - 1 } else { 0 });
     }
     for i in 0..w {
         m[get(i, 0)] = i;
+        backtrack[get(i, 0)] = EditType::Insert(if i > 0 { i - 1 } else { 0 });
     }
+    backtrack[get(0, 0)] = EditType::Unknown;
     // Compute
-    let v2: Vec<(i32, i32)> = vec![(-1, 0), (0, -1), (-1, -1)];
+    let v2 = vec![(-1, 0), (0, -1), (-1, -1)];
     for y in 1..(s1.len() + 1) {
         for x in 1..(s2.len() + 1) {
-            let substitution_cost = if s1[y - 1] == s2[x - 1] { 0 } else { 1 };
+            let substitution_cost = if s1[y - 1] == s2[x - 1] { 0 } else { 2 };
             let v = vec![
                 m[get(x - 1, y)] + 1,
                 m[get(x, y - 1)] + 1,
                 m[get(x - 1, y - 1)] + substitution_cost,
             ];
             let i = min_arg(&v);
-            backtrack[get(x, y)] = v2[i];
+            let v3 = match i {
+                0 => EditType::Insert(x - 1),
+                1 => EditType::Delete(y - 1),
+                2 => EditType::Substitute(y - 1, x - 1, substitution_cost == 0),
+                _ => EditType::Unknown,
+            };
+            backtrack[get(x, y)] = v3;
+            backtrack2[get(x, y)] = v2[i];
             let mn = v[i];
             m[get(x, y)] = mn;
         }
@@ -287,10 +328,24 @@ fn edit_distance<T: std::cmp::Eq>(s1: Vec<T>, s2: Vec<T>) -> Vec<Vec<usize>> {
     for y in 0..h {
         let mut t = Vec::new();
         for x in 0..w {
-            t.push(backtrack[get(x, y)]);
+            t.push(backtrack2[get(x, y)]);
         }
         m4.push(t);
     }
+    let mut pos = (w - 1, h - 1);
+    let mut n = &backtrack[get(pos.0, pos.1)];
+    let mut r2: Vec<&EditType> = Vec::new();
+    while !matches!(n, EditType::Unknown) {
+        r2.push(n);
+        match n {
+            EditType::Insert(_) => pos = (pos.0 - 1, pos.1),
+            EditType::Delete(_) => pos = (pos.0, pos.1 - 1),
+            EditType::Substitute(_, _, _) => pos = (pos.0 - 1, pos.1 - 1),
+            EditType::Unknown => pos = (0, 0),
+        }
+        n = &backtrack[get(pos.0, pos.1)];
+    }
+    println!("{:?}", r2);
     let f: fn(&Vec<(i32, i32)>) -> String = |m| {
         m.iter()
             .map(|x| format!("({}, {})", x.0, x.1))
@@ -437,7 +492,7 @@ mod tests {
 
     #[test]
     fn test_edit_distance() -> Result<()> {
-        let res = edit_distance("an act".chars().collect(), "a cat".chars().collect());
+        let res = edit_distance("2334".chars().collect(), "1223344".chars().collect());
         let f: fn(&Vec<usize>) -> String = |m| {
             m.iter()
                 .map(|x| x.to_string())
