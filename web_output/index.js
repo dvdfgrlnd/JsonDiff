2d7929e61cef9ea4ba19080e9c48d29d0bbe47e2
david <dvdfgrlnd@gmail.com>
2021-10-04 11:07:12 +0200

    Add web output

---
+++ web_output/index.js
@@ -0,0 +1,43 @@
+import init, { find_diff } from "./jsondiff.js";
+
+
+let r = `
+            {
+            "f1": "v2",
+            "f8": "v0",
+            "f9": false,
+            "f2": {
+                "f3": 456,
+                "f4": {
+                    "f0":123
+                }
+            },
+            "f3": {
+                "f5": "v6"
+            }
+        }`;
+let r2 = `
+            {
+            "f1": "v2",
+            "f8": "v1",
+            "f9": true,
+            "f2": {
+                "f3": 456,
+                "f4": {
+                    "f0":122
+                }
+            },
+            "f3": "v6"
+        }`;
+init()
+    .then(() => {
+        document.getElementById("computediff").addEventListener("click", function () {
+            let r1 = document.getElementById("text1").value;
+            let r2 = document.getElementById("text2").value;
+            if (r1.length > 0 && r2.length > 0) {
+                let output = find_diff(r1, r2);
+                console.log("output", output);
+                document.getElementById("jsonresult").innerHTML = output;
+            }
+        });
+    });
---
+++ web_output/jsondiff.d.ts
@@ -0,0 +1,29 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} arg1
+* @param {string} arg2
+* @returns {string}
+*/
+export function find_diff(arg1: string, arg2: string): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly find_diff: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
